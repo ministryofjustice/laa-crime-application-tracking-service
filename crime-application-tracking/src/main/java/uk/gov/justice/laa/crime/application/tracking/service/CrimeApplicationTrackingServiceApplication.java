b2a9596471f321e6fd62da7c3d48e530f83df162
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-10-25 17:59:09 +0100

    Initial Repo Setup

---
+++ crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/service/CrimeApplicationTrackingServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.application.tracking.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeApplicationTrackingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeApplicationTrackingServiceApplication.class, args);
+    }
+
+}
---
+++ crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/service/CrimeApplicationTrackingServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package uk.gov.justice.laa.crime.application.tracking.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CrimeApplicationTrackingServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
