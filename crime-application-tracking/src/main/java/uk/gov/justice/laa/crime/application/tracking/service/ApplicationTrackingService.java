607ebbfb02a27b6d04435fd8249a5df1baa89d5c
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2024-03-15 11:07:53 +0000

    Refactoring changes

--- crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/service/ApplicationTrackingService.java
+++ crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/service/ApplicationTrackingService.java
@@ -5,8 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult;
 
-import java.util.Objects;
-
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -18,24 +16,23 @@ public class ApplicationTrackingService {
     private final ApplicationOutputResultService applicationOutputResultService;
 
     public void processApplicationTrackingAndOutputResultData(ApplicationTrackingOutputResult applicationTrackingOutputResult) {
-
-        if (Objects.nonNull(applicationTrackingOutputResult.getUsn())
-                && Objects.nonNull(applicationTrackingOutputResult.getRequestSource())) {
-            ApplicationTrackingOutputResult.RequestSource requestSource = applicationTrackingOutputResult.getRequestSource();
-            switch (requestSource) {
-                case CREATE_APPLICATION -> {
-                    eformAuditService.createAudit(applicationTrackingOutputResult);
-                    eformStagingService.updateMaatId(applicationTrackingOutputResult.getUsn(), applicationTrackingOutputResult.getMaatRef());
-                    eformsHistoryService.createEformHistory(applicationTrackingOutputResult);
-                    applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
-                }
-                case HARDSHIP, CROWN_COURT -> applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
-                case CAPITAL_AND_EQUITY -> eformStagingService.updateStatus(applicationTrackingOutputResult.getUsn());
-                case ASSESSMENTS, PASSPORT_IOJ -> {
-                    eformsHistoryService.createEformHistory(applicationTrackingOutputResult);
-                    applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
-                }
+        Integer usn = applicationTrackingOutputResult.getUsn();
+        ApplicationTrackingOutputResult.RequestSource requestSource = applicationTrackingOutputResult.getRequestSource();
+        log.info("Start process to handle Application Tracking and Output Result for {} with USN {}", requestSource.value(), usn);
+        switch (requestSource) {
+            case CREATE_APPLICATION -> {
+                eformAuditService.createAudit(applicationTrackingOutputResult);
+                eformStagingService.updateMaatId(usn, applicationTrackingOutputResult.getMaatRef());
+                eformsHistoryService.createEformHistory(applicationTrackingOutputResult);
+                applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
+            }
+            case HARDSHIP, CROWN_COURT -> applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
+            case CAPITAL_AND_EQUITY -> eformStagingService.updateStatus(usn);
+            case PASSPORT_IOJ, MEANS_ASSESSMENT -> {
+                eformsHistoryService.createEformHistory(applicationTrackingOutputResult);
+                applicationOutputResultService.processOutputResult(applicationTrackingOutputResult);
             }
         }
+        log.info("Successfully processed Application Tracking and Output Result for {} with USN {}", requestSource.value(), usn);
     }
 }
