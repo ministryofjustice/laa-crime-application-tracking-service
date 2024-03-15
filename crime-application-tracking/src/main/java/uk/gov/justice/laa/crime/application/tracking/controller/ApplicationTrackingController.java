08046225f007d645508e6b29a7bc1d982b86bef1
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2024-03-15 17:27:16 +0000

    uodated schema

--- crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/controller/ApplicationTrackingController.java
+++ crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/controller/ApplicationTrackingController.java
@@ -3,7 +3,6 @@ package uk.gov.justice.laa.crime.application.tracking.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
@@ -12,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult;
@@ -43,9 +43,7 @@ public class ApplicationTrackingController {
     )
 
     public ResponseEntity<Void> processApplicationTrackingAndOutputResult(@RequestBody ApplicationTrackingOutputResult applicationTrackingOutputResult) {
-
         applicationTrackingService.processApplicationTrackingAndOutputResultData(applicationTrackingOutputResult);
-
         return ResponseEntity.ok().build();
     }
 }
