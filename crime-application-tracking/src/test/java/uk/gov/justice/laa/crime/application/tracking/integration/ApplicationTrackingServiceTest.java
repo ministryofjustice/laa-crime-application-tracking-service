bced14eeab3c53156f29ea49b4717d70c479c4a7
Kevin-Paxton <kevin.paxton@digital.justice.gov.uk>
2024-04-25 12:04:39 +0100

    Integration tests

---
+++ crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/integration/ApplicationTrackingServiceTest.java
@@ -0,0 +1,279 @@
+package uk.gov.justice.laa.crime.application.tracking.integration;
+
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import uk.gov.justice.laa.crime.application.tracking.CrimeApplicationTrackingApplication;
+import uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult;
+import uk.gov.justice.laa.crime.application.tracking.testutils.FileUtils;
+import uk.gov.justice.laa.crime.application.tracking.testutils.JsonUtils;
+import uk.gov.justice.laa.crime.application.tracking.util.MockWebServerStubs;
+
+import java.io.IOException;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult.RequestSource.*;
+
+@SpringBootTest(classes = CrimeApplicationTrackingApplication.class, webEnvironment = DEFINED_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ApplicationTrackingServiceTest {
+    private MockMvc mvc;
+
+    private static MockWebServer mockWebServer;
+    private String content;
+
+    private ApplicationTrackingOutputResult applicationTrackingOutputResult;
+
+    @Autowired private WebApplicationContext webApplicationContext;
+
+    @BeforeAll
+    public void initialiseMockWebServer() throws IOException {
+        content = FileUtils.readFileToString("testdata/atsrequest.json");
+        applicationTrackingOutputResult = JsonUtils.jsonToObject(content, ApplicationTrackingOutputResult.class);
+
+        mockWebServer = new MockWebServer();
+        mockWebServer.setDispatcher(MockWebServerStubs.forDownstreamApiCalls());
+        mockWebServer.start(9999);
+    }
+
+    @AfterAll
+    protected void shutdownMockWebServer() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @BeforeEach
+    public void setup() {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldCreateAudit_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CREATE_APPLICATION);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotCreateAudit_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CREATE_APPLICATION);
+        applicationTrackingOutputResult.setUsn(123456);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldProcessHardship_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(HARDSHIP);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotProcessHardship_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(HARDSHIP);
+        applicationTrackingOutputResult.setUsn(123456);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldProcessCrownCourt_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CROWN_COURT);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotProcessCrownCourt_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CROWN_COURT);
+        applicationTrackingOutputResult.setUsn(123456);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldUpdateCapitalAndEquity_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CAPITAL_AND_EQUITY);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotUpdateCapitalAndEquity_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(CAPITAL_AND_EQUITY);
+        applicationTrackingOutputResult.setUsn(123456);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldProcessPassportIOJ_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(PASSPORT_IOJ);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotProcessPassportIOJ_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(PASSPORT_IOJ);
+        applicationTrackingOutputResult.setUsn(12345);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldProcessMeansAssesment_andDoRequiredUpdates() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(MEANS_ASSESSMENT);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+    }
+
+    @Test
+    void givenCreateApplicationRequest_shouldNotProcessMeansAssesment_andThrowError() throws Exception {
+        applicationTrackingOutputResult.setRequestSource(MEANS_ASSESSMENT);
+        applicationTrackingOutputResult.setUsn(123456);
+        String string = JsonUtils.objectToJson(applicationTrackingOutputResult);
+        RequestBuilder request =
+                MockMvcRequestBuilders.post(
+                                "/api/internal/v1/application-tracking-output-result")
+                        .content(string)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result =
+                mvc.perform(request)
+                        .andExpect(status().is4xxClientError())
+                        .andReturn();
+
+    }
+}
---
+++ crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/util/MockWebServerStubs.java
@@ -0,0 +1,125 @@
+package uk.gov.justice.laa.crime.application.tracking.util;
+
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import uk.gov.justice.laa.crime.application.tracking.testutils.FileUtils;
+
+import javax.validation.constraints.NotNull;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class MockWebServerStubs {
+
+  private static final Map<String, RequestPathResponseMapping> PATH_TO_RESPONSE_MAPPING_MAP =
+      Arrays.stream(RequestPathResponseMapping.values())
+          .collect(
+              Collectors.toMap(
+                  RequestPathResponseMapping::getRequestPath,
+                  requestPathResponseMapping -> requestPathResponseMapping));
+
+  public static Dispatcher forDownstreamApiCalls() {
+    return new Dispatcher() {
+      @NotNull @Override
+      public MockResponse dispatch(@NotNull RecordedRequest recordedRequest) {
+        String requestPath = recordedRequest.getPath();
+        RequestPathResponseMapping responseMapping = PATH_TO_RESPONSE_MAPPING_MAP.get(requestPath);
+        return responseMapping.getMockResponse();
+      }
+    };
+  }
+
+  enum RequestPathResponseMapping {
+    OAUTH2(
+            "/oauth2/token",
+            "testdata/OAuth2Response.json",
+            HttpStatus.OK
+    ),
+    EFORM_AUDIT(
+            "/eform/audit",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM(
+            "/eform/7264893",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM_HISTORY(
+            "/eform/history",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM_DECISION_HISTORY(
+"/eform/decision-history",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM_PREVIOUS_DECISION_HOSTORY(
+            "/eform/decision-history/7264893/previous-wrote-to-result",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM_DECISION_HISTORY_USN(
+            "/eform/decision-history/7264893",
+            null,
+            HttpStatus.OK
+    ),
+    EFORM_RESULTS(
+            "/eform/results",
+            null,
+            HttpStatus.OK
+    ),
+    INTERNAL_FINANCIAL_CHECK_OUTSTANDING(
+            "/internal/v1/assessment/financial-assessments/check-outstanding/73856111",
+            null,
+            HttpStatus.OK
+    ),
+    INTERNAL_REP_ORDER_IOJ_ASSESOR_DETAILS(
+            "/internal/v1/assessment/rep-orders/73856111/ioj-assessor-details",
+            null,
+            HttpStatus.OK
+    ),
+    INTERNAL_FINANCIAL_MEANS_ASSESOR_DETAILS(
+            "/internal/v1/assessment/financial-assessments/85478545/means-assessor-details",
+            null,
+            HttpStatus.OK
+    ),
+    INTERNAL_PASSPORT_ASSESOR_DETAILS(
+            "/internal/v1/assessment/passport-assessments/98658658/passport-assessor-details",
+            null,
+            HttpStatus.OK
+    );
+
+    private final String requestPath;
+    private final String responseBodyFilePath;
+    private final HttpStatus httpResponseStatus;
+
+    RequestPathResponseMapping(
+        String requestPath, String responseBodyFilePath, HttpStatus httpResponseStatus) {
+      this.requestPath = requestPath;
+      this.responseBodyFilePath = responseBodyFilePath;
+      this.httpResponseStatus = httpResponseStatus;
+    }
+
+    String getRequestPath() {
+      return requestPath;
+    }
+
+    MockResponse getMockResponse() {
+      MockResponse mockResponse =
+          new MockResponse()
+              .addHeader("Content-Type", MediaType.APPLICATION_JSON)
+              .setResponseCode(httpResponseStatus.value());
+
+      if (StringUtils.isNotBlank(responseBodyFilePath)) {
+        mockResponse.setBody(FileUtils.readFileToString(responseBodyFilePath));
+      }
+      return mockResponse;
+    }
+  }
+}
