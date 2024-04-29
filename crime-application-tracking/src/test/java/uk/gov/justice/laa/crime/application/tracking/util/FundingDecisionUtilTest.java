b802e2b16326a887b5aece4660b21bd52561e8cd
Kevin-Paxton <kevin.paxton@digital.justice.gov.uk>
2024-04-29 17:09:32 +0100

    Update FundingDecisionUtilTest.java

--- crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/util/FundingDecisionUtilTest.java
+++ crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/util/FundingDecisionUtilTest.java
@@ -1,8 +1,5 @@
 package uk.gov.justice.laa.crime.application.tracking.util;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -14,20 +11,27 @@ import uk.gov.justice.laa.crime.application.tracking.model.Passport;
 import uk.gov.justice.laa.crime.application.tracking.model.Passport.PassportResult;
 import uk.gov.justice.laa.crime.application.tracking.testutils.TestData;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FundingDecisionUtilTest {
 
     private static final String GRANTED_PASSED_MEANS_TEST = "Granted - Passed Means Test";
     private static final String GRANTED = "Granted";
+    private static final Object REFUSED_INELIGIBLE = "Refused - Ineligible";
+    private static final String FAILED_CF_S_FAILED_MEANS_TEST = "Failed - CfS Failed Means Test";
+
 
     @ParameterizedTest
     @MethodSource("testDataForFundingDecision")
     void shouldReturnFundingDecisionForIndictableCaseType(
-        ApplicationTrackingOutputResult.AssessmentType assessmentType,
-        Passport.PassportResult passportResult,
-        ApplicationTrackingOutputResult.CaseType caseType,
-        MeansAssessment.MeansAssessmentResult meansAssessmentResult,
-        String ccRepDecision,
-        String expectedFundDecision) {
+            ApplicationTrackingOutputResult.AssessmentType assessmentType,
+            Passport.PassportResult passportResult,
+            ApplicationTrackingOutputResult.CaseType caseType,
+            MeansAssessment.MeansAssessmentResult meansAssessmentResult,
+            String ccRepDecision,
+            String expectedFundDecision) {
 
 
         var atsRequest = TestData.getAtsRequest();
@@ -35,34 +39,107 @@ class FundingDecisionUtilTest {
         atsRequest.setAssessmentType(assessmentType);
         atsRequest.getPassport().setPassportResult(passportResult);
         atsRequest.getMeansAssessment().setMeansAssessmentResult(meansAssessmentResult);
-        atsRequest.setCcRepDecision(ccRepDecision);
+        if(ccRepDecision != null) {
+            atsRequest.setCcRepDecision(ccRepDecision);
+        }
 
         String actualFundingDecision = FundingDecisionUtil.getFundingDecision(atsRequest);
 
         assertEquals(expectedFundDecision, actualFundingDecision);
     }
+
     private static Stream<Arguments> testDataForFundingDecision() {
         return Stream.of(
-            Arguments.of(
-                AssessmentType.MEANS_FULL,
-                PassportResult.FAIL,
-                ApplicationTrackingOutputResult.CaseType.INDICTABLE,
-                MeansAssessmentResult.FULL,
-                "CcRepDecision",
-                GRANTED_PASSED_MEANS_TEST),
-            Arguments.of(
-                AssessmentType.MEANS_FULL,
-                PassportResult.FAIL,
-                ApplicationTrackingOutputResult.CaseType.COMMITAL,
-                MeansAssessmentResult.FULL,
-                "CcRepDecision",
-                GRANTED),
-            Arguments.of(AssessmentType.MEANS_FULL,
-                PassportResult.FAIL,
-                ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
-                MeansAssessmentResult.FULL,
-                "CcRepDecision",
-                GRANTED)
+                // Test cases for different AssessmentTypes and CaseTypes
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.INDICTABLE,
+                        MeansAssessmentResult.FULL,
+                        null,
+                        GRANTED_PASSED_MEANS_TEST),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.COMMITAL,
+                        MeansAssessmentResult.FULL,
+                        null,
+                        GRANTED),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
+                        MeansAssessmentResult.FULL,
+                        null,
+                        GRANTED),
+                // Additional test cases for different combinations
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.PASS,
+                        ApplicationTrackingOutputResult.CaseType.INDICTABLE,
+                        MeansAssessmentResult.FULL,
+                        null,
+                        GRANTED_PASSED_MEANS_TEST),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.PASS,
+                        ApplicationTrackingOutputResult.CaseType.COMMITAL,
+                        MeansAssessmentResult.FULL,
+                        null,
+                        GRANTED),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.PASS,
+                        ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
+                        MeansAssessmentResult.FULL,
+                        "CcRepDecision",
+                        GRANTED),
+                // Test cases for different MeansAssessmentResults
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.INDICTABLE,
+                        MeansAssessmentResult.INEL,
+                        "CcRepDecision",
+                        REFUSED_INELIGIBLE),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.COMMITAL,
+                        MeansAssessmentResult.INEL,
+                        "Failed - CfS Failed Means Test",
+                        FAILED_CF_S_FAILED_MEANS_TEST),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
+                        MeansAssessmentResult.INEL,
+                        "CcRepDecision",
+                        REFUSED_INELIGIBLE),
+                // Test cases for different ccRepDecision values
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.INDICTABLE,
+                        MeansAssessmentResult.FULL,
+                        "Granted - Passed Means Test",
+                        GRANTED_PASSED_MEANS_TEST),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.COMMITAL,
+                        MeansAssessmentResult.FULL,
+                        "Granted - Passed Means Test",
+                        GRANTED),
+                Arguments.of(
+                        AssessmentType.MEANS_FULL,
+                        PassportResult.FAIL,
+                        ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
+                        MeansAssessmentResult.FULL,
+                        "Granted - Passed Means Test",
+                        GRANTED)
+                // Additional test cases to cover more scenarios
+                // ...
         );
     }
 }
