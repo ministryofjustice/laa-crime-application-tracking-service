aa31ae3320dcda3ccd718c823671d03f52479f83
Alex Stone <alex.stone@digital.justice.gov.uk>
2024-04-29 15:37:38 +0100

    Additional args

--- crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/util/FundingDecisionUtilTest.java
+++ crime-application-tracking/src/test/java/uk/gov/justice/laa/crime/application/tracking/util/FundingDecisionUtilTest.java
@@ -1,13 +1,18 @@
 package uk.gov.justice.laa.crime.application.tracking.util;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult;
+import uk.gov.justice.laa.crime.application.tracking.model.ApplicationTrackingOutputResult.AssessmentType;
+import uk.gov.justice.laa.crime.application.tracking.model.MeansAssessment;
+import uk.gov.justice.laa.crime.application.tracking.model.MeansAssessment.MeansAssessmentResult;
+import uk.gov.justice.laa.crime.application.tracking.model.Passport;
+import uk.gov.justice.laa.crime.application.tracking.model.Passport.PassportResult;
 import uk.gov.justice.laa.crime.application.tracking.testutils.TestData;
-
-import java.util.stream.Stream;
 
 class FundingDecisionUtilTest {
 
@@ -16,18 +21,48 @@ class FundingDecisionUtilTest {
 
     @ParameterizedTest
     @MethodSource("testDataForFundingDecision")
-    void shouldReturnFundingDecisionForIndictableCaseType(ApplicationTrackingOutputResult.CaseType caseType, String expectedFundDecision){
+    void shouldReturnFundingDecisionForIndictableCaseType(
+        ApplicationTrackingOutputResult.AssessmentType assessmentType,
+        Passport.PassportResult passportResult,
+        ApplicationTrackingOutputResult.CaseType caseType,
+        MeansAssessment.MeansAssessmentResult meansAssessmentResult,
+        String ccRepDecision,
+        String expectedFundDecision) {
+
+
         var atsRequest = TestData.getAtsRequest();
         atsRequest.setCaseType(caseType);
-        String actualFundingDecision = FundingDecisionUtil.getFundingDecision(atsRequest);
-        Assertions.assertEquals(expectedFundDecision, actualFundingDecision);
+        atsRequest.setAssessmentType(assessmentType);
+        atsRequest.getPassport().setPassportResult(passportResult);
+        atsRequest.getMeansAssessment().setMeansAssessmentResult(meansAssessmentResult);
+        atsRequest.setCcRepDecision(ccRepDecision);
 
+        String actualFundingDecision = FundingDecisionUtil.getFundingDecision(atsRequest);
+
+        assertEquals(expectedFundDecision, actualFundingDecision);
     }
     private static Stream<Arguments> testDataForFundingDecision() {
         return Stream.of(
-                Arguments.of(ApplicationTrackingOutputResult.CaseType.INDICTABLE, GRANTED_PASSED_MEANS_TEST),
-                Arguments.of(ApplicationTrackingOutputResult.CaseType.COMMITAL, GRANTED),
-                Arguments.of(ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY, GRANTED)
+            Arguments.of(
+                AssessmentType.MEANS_FULL,
+                PassportResult.FAIL,
+                ApplicationTrackingOutputResult.CaseType.INDICTABLE,
+                MeansAssessmentResult.FULL,
+                "CcRepDecision",
+                GRANTED_PASSED_MEANS_TEST),
+            Arguments.of(
+                AssessmentType.MEANS_FULL,
+                PassportResult.FAIL,
+                ApplicationTrackingOutputResult.CaseType.COMMITAL,
+                MeansAssessmentResult.FULL,
+                "CcRepDecision",
+                GRANTED),
+            Arguments.of(AssessmentType.MEANS_FULL,
+                PassportResult.FAIL,
+                ApplicationTrackingOutputResult.CaseType.SUMMARY_ONLY,
+                MeansAssessmentResult.FULL,
+                "CcRepDecision",
+                GRANTED)
         );
     }
 }
