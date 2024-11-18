ba8ff7c6428e49a5424044b46d3c42a3da86dacd
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2024-11-18 09:38:10 +0000

    Restore normal functionality

--- crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/CrimeApplicationTrackingApplication.java
+++ crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/CrimeApplicationTrackingApplication.java
@@ -11,17 +11,6 @@ public class CrimeApplicationTrackingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrimeApplicationTrackingApplication.class, args);
-        for (int i = 0; i < 20; i++) {
-            try {
-                // Intentionally cause an error
-                throw new RuntimeException("Test error " + (i + 1));
-            } catch (RuntimeException e) {
-                // Capture the error with Sentry
-                Sentry.captureException(e);
-                // Handle the error (e.g., log it)
-                System.err.println("Caught error: " + e.getMessage());
-            }
-        }
 
     }
 
