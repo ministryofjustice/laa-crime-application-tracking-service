f3b82deaab0a9014372b1ed223a3041341f6f03a
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2024-11-18 09:12:31 +0000

    Test Sentry Alerting

--- crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/CrimeApplicationTrackingApplication.java
+++ crime-application-tracking/src/main/java/uk/gov/justice/laa/crime/application/tracking/CrimeApplicationTrackingApplication.java
@@ -3,6 +3,7 @@ package uk.gov.justice.laa.crime.application.tracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import io.sentry.Sentry;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -10,6 +11,18 @@ public class CrimeApplicationTrackingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CrimeApplicationTrackingApplication.class, args);
+        for (int i = 0; i < 20; i++) {
+            try {
+                // Intentionally cause an error
+                throw new RuntimeException("Test error " + (i + 1));
+            } catch (RuntimeException e) {
+                // Capture the error with Sentry
+                Sentry.captureException(e);
+                // Handle the error (e.g., log it)
+                System.err.println("Caught error: " + e.getMessage());
+            }
+        }
+
     }
 
 }
